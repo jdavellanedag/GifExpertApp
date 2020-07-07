05ae4b7969e731b5528e81b522039d4912269026
jdavellanedag <juldave.gal@hotmail.com>
2020-07-06 23:35:33 -0500

    Intial version

---
+++ src/components/AddCategory.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+export const AddCategory = ({ setCategories }) => {
+  const [inputValue, setinputValue] = useState("");
+
+  const hangleInputChange = (e) => {
+    setinputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.trim().length > 2) {
+      setCategories((cats) => [inputValue, ...cats]);
+      setinputValue("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={inputValue} onChange={hangleInputChange} />
+    </form>
+  );
+};
+
+AddCategory.propTypes = {
+  setCategories: PropTypes.func.isRequired,
+};
