e6eb8507aa99b8c22af2b7c30ae4b4e77b823c3e
jdavellanedag <juldave.gal@hotmail.com>
2020-07-08 20:20:09 -0500

    Git docs

---
+++ docs/precache-manifest.7320942e4e3189192aad50f3787d4e5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b916feef8e934feeff2704b74b1e8ede",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ec79099fdcb47bdada7",
+    "url": "/static/css/main.b19c32af.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ec79099fdcb47bdada7",
+    "url": "/static/js/main.2f4232e2.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
